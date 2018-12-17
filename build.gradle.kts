265f143d586a749de41be36c5677478570777e54
wooin.joe <jwj0831@gmail.com>
2018-12-17 18:03:48 +0900

    Create built-in task

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,11 @@
  * This is a general purpose Gradle build.
  * Learn how to create Gradle builds at https://guides.gradle.org/creating-new-gradle-builds/
  */
+
+// https://docs.gradle.org/5.0/dsl/org.gradle.api.tasks.Exec.html
+tasks.create<Exec>("list-up") {
+    description = "Execute Test shell command"
+    group = "My own group"
+
+    commandLine("ls", "-al")
+}
